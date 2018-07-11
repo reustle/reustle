28862492cc0f6b6c78ea68306f3412d705072f5c
Shane Reustle <me@shanereustle.com>
2018-07-11 16:40:01 +0200

    geojson file powered rtw map

--- static/js/rtw.js
+++ static/js/rtw.js
@@ -1,44 +1,23 @@
+"use strict";
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicmV1c3RsZSIsImEiOiJESzd6YVRnIn0.Hh9AwQw1X0PR_TOewZMMzA';
 
 // Utils
 
-var GREEN = '#35d835';
-var BLUE = '#4EADE4';
-var ORANGE = '#da870a';
+let GREEN = '#35d835'
+let BLUE = '#4eade4'
+let ORANGE = '#da870a'
 
 var COLORS = {
     'train': BLUE,
-    'bus': GREEN,
-    'taxi': GREEN,
-    'boat': ORANGE,
-    'ferry': ORANGE,
+    'bus': ORANGE,
+    'taxi': ORANGE,
+    'car': ORANGE,
+    'boat': GREEN,
 };
 
-var tripsToFeatures = function(trips){
-    // Convert a list of json to geojson features
-    
-    var cleanTrips = [];
-    trips.forEach(function(trip){
-        var tripColor = COLORS[trip.mode] || BLUE;
-
-        cleanTrips.push({
-            type: 'Feature',
-            properties: {
-                color: tripColor
-
-            },
-            geometry: {
-                type: 'LineString',
-                coordinates: trip.coords
-            }
-        });
-    });
-    
-    return cleanTrips;
-};
 
 // Init Map
-
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v9',
@@ -58,28 +37,38 @@ map.touchZoomRotate.disableRotation();
 
 // Init map
 map.on('load', function(){
-    map.addSource('trips', {
-        type: 'geojson',
-        data: {
-            type: 'FeatureCollection',
-            features: tripsToFeatures(rawTrips)
-        }
-    });
-    map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'trips',
-        layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        paint: {
-            'line-width': 3,
-            'line-color': {
-                'type': 'identity',
-                'property': 'color'
-            }
-        }
-    });
+    
+    // Load json data
+    // TODO Async
+    $.getJSON("/static/rtwData.geojson", function(rtwData){
+        rtwData.features.forEach(feature => {
+            feature.properties.color = COLORS[feature.properties.name.toLowerCase()]
+        })
+        
+        map.addLayer({
+            id: "route",
+            type: "line",
+            
+            source: {
+                type: "geojson",
+                data: rtwData
+            },
+            
+            paint: {
+                'line-width': 3,
+                'line-color': {
+                    'type': 'identity',
+                    'property': 'color'
+                }
+            },
+            
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+
+        })
+        
+    })
 });
 
