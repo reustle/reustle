de92b7436bc2ed5676024a978b9dbe55cd08ae38
Shane Reustle <me@shanereustle.com>
2018-07-29 20:50:48 +0300

    fade secondary rtw lines

--- static/js/rtw.js
+++ static/js/rtw.js
@@ -44,23 +44,65 @@ map.on('load', function(){
     // Load json data
     // TODO Async
     $.getJSON("/static/rtwData.geojson", function(rtwData){
+        let rtwDataPrimary = []
+        let rtwDataOther = []
         rtwData.features.forEach(feature => {
             let nameProp = feature.properties.name.toLowerCase().split(',')
             let transportMode = nameProp[0]
             feature.properties.color = COLORS[transportMode]
+            
+            if(feature.properties.rtw && feature.properties.rtw == 'true'){
+                rtwDataPrimary.push(feature)
+            }else{
+                rtwDataOther.push(feature)
+            }
         })
         
+        // Add primary rtw line
         map.addLayer({
-            id: "route",
+            id: "routePrimary",
             type: "line",
             
             source: {
                 type: "geojson",
-                data: rtwData
+                data: {
+                    type: "FeatureCollection",
+                    features: rtwDataPrimary
+                }
+            },
+            
+            paint: {
+                'line-width': 3,
+                'line-color': {
+                    'type': 'identity',
+                    'property': 'color'
+                }
+            },
+            
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+
+        })
+        
+        // Add secondary lines
+        map.addLayer({
+            id: "routeOther",
+            type: "line",
+            
+            source: {
+                type: "geojson",
+                data: {
+                    type: "FeatureCollection",
+                    features: rtwDataOther
+                }
             },
             
             paint: {
                 'line-width': 2,
+                'line-opacity': 0.8,
+                'line-blur': 1,
                 'line-color': {
                     'type': 'identity',
                     'property': 'color'
