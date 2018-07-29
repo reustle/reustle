8f2eea658c5b4d56fae21f4f36082fab1f8cdaaf
Shane Reustle <me@shanereustle.com>
2018-07-29 23:35:59 +0300

    new colors

--- static/js/rtw.js
+++ static/js/rtw.js
@@ -4,24 +4,24 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmV1c3RsZSIsImEiOiJESzd6YVRnIn0.Hh9AwQw1X0PR_
 
 // Utils
 
-let GREEN = '#94e86f'
-let BLUE = '#4eade4'
-let ORANGE = '#da870a'
+let ROAD = '#ad9553'
 
 var COLORS = {
-    'train': BLUE,
-    'bus': ORANGE,
-    'taxi': ORANGE,
-    'car': ORANGE,
-    'boat': GREEN,
-    'ferry': GREEN,
+    'train': '#43a1d8',
+    'boat': '#1f648c',
+    //'road': '',
+    'bus': ROAD,
+    'taxi': ROAD,
+    'car': ROAD,
 };
 
 
 // Init Map
 var map = new mapboxgl.Map({
     container: 'map',
+    //style: 'mapbox://styles/mapbox/satellite-v9',
     style: 'mapbox://styles/mapbox/dark-v9',
+    //style: 'mapbox://styles/reustle/cjk79zycy0by52so62irk2j4m',
     zoom: 1.5,
     minZoom: 1,
     maxZoom: 5,
@@ -44,18 +44,10 @@ map.on('load', function(){
     // Load json data
     // TODO Async
     $.getJSON("/static/rtwData.geojson", function(rtwData){
-        let rtwDataPrimary = []
-        let rtwDataOther = []
         rtwData.features.forEach(feature => {
-            let nameProp = feature.properties.name.toLowerCase().split(',')
-            let transportMode = nameProp[0]
+            let modeProp = feature.properties.mode.toLowerCase().split(',')
+            let transportMode = modeProp[0]
             feature.properties.color = COLORS[transportMode]
-            
-            if(feature.properties.rtw && feature.properties.rtw == 'true'){
-                rtwDataPrimary.push(feature)
-            }else{
-                rtwDataOther.push(feature)
-            }
         })
         
         // Add primary rtw line
@@ -65,44 +57,12 @@ map.on('load', function(){
             
             source: {
                 type: "geojson",
-                data: {
-                    type: "FeatureCollection",
-                    features: rtwDataPrimary
-                }
+                data: rtwData
             },
             
             paint: {
-                'line-width': 3,
-                'line-color': {
-                    'type': 'identity',
-                    'property': 'color'
-                }
-            },
-            
-            layout: {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-
-        })
-        
-        // Add secondary lines
-        map.addLayer({
-            id: "routeOther",
-            type: "line",
-            
-            source: {
-                type: "geojson",
-                data: {
-                    type: "FeatureCollection",
-                    features: rtwDataOther
-                }
-            },
-            
-            paint: {
-                'line-width': 2,
-                'line-opacity': 0.8,
-                'line-blur': 1,
+                'line-width': 2.5,
+                'line-blur': 0,
                 'line-color': {
                     'type': 'identity',
                     'property': 'color'
