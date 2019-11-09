50f1616bf55869672512c46f9c2b15a4519aef4d
Shane Reustle <shane@reustle.co>
2019-11-09 23:18:47 +0000

    cleaned up cambodia and to bkk

--- update-rtw-data.sh
+++ update-rtw-data.sh
@@ -1,4 +1,6 @@
 wget https://gist.githubusercontent.com/reustle/6d27b7f01a98bffb76b150dc860460eb/raw/map.geojson -O static/rawRtwData.geojson ;
 jq -c . < static/rawRtwData.geojson > static/rtwData.geojson ;
-rm static/rawRtwData.geojson
+rm static/rawRtwData.geojson ;
+git add static/rtwData.geojson ;
+git status
 
